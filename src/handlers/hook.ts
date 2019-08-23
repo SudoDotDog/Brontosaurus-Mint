5e2354fc448afcc5c2badcd26ab71241482f25a2
WMXPY <wm@mengw.io>
2019-08-23 15:58:50 -0500

    add autohook

--- src/handlers/hook.ts
+++ src/handlers/hook.ts
@@ -29,3 +29,24 @@ export const basicHook: SudooExpressHook<[string, boolean?]> =
             return !isFailed;
         });
 
+export const autoHook: SudooExpressHook<[string, boolean?]> =
+    SudooExpressHook.create<[string, boolean?]>()
+        .before((req: SudooExpressRequest, res: SudooExpressResponse, content: string, isInfo: boolean = false): boolean => {
+
+            const log: SudooLog = SudooLog.global;
+            const isFailed: boolean = res.agent.isFailed();
+
+            const parsedContent = `${content}: ${
+                isFailed
+                    ? 'Failed'
+                    : 'Entered'
+                }`;
+
+            if (isInfo) {
+                log.info(`${req.path} - ${parsedContent}`);
+            } else {
+                log.verbose(`${req.path} - ${parsedContent}`);
+            }
+
+            return !isFailed;
+        });
