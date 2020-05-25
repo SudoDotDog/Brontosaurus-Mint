a37ba1931d16ee518cb37da1d29dcf21ed914f76
WMXPY <wm@mengw.io>
2020-05-25 16:50:06 -0500

    remove unused broken function

--- src/util/token.ts
+++ src/util/token.ts
@@ -4,8 +4,7 @@
  * @description Token
  */
 
-import { BrontosaurusSign } from "@brontosaurus/core";
-import { IApplicationModel, INFOS_SPLITTER } from "@brontosaurus/db";
+import { INFOS_SPLITTER } from "@brontosaurus/db";
 import { Basics, IBrontosaurusBody, IBrontosaurusHeader } from "@brontosaurus/definition";
 import { _Json } from "@sudoo/bark/json";
 import { _Map } from "@sudoo/bark/map";
@@ -24,17 +23,6 @@ export const jsonifyBasicRecords = (infoLine: string | Record<string, Basics>, e
     }
 
     return infoLine;
-};
-
-export const createToken = (body: IBrontosaurusBody, application: IApplicationModel): string => {
-
-    const sign: BrontosaurusSign = BrontosaurusSign.create(application.key, body, {
-        public: application.publicKey,
-        private: application.privateKey,
-    });
-    const token: string = sign.token(Date.now() + application.expire);
-
-    return token;
 };
 
 export const parseInfo = (infoRecord: Record<string, Basics>): string[] => {
