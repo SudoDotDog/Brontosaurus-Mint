5dfa540d0bc87523ebd057ba7111e33204dc5379
WMXPY <wm@mengw.io>
2020-07-19 14:41:07 -0500

    add generateor related pattern

--- src/routes/account/generate-temp-password.ts
+++ src/routes/account/generate-temp-password.ts
@@ -5,13 +5,14 @@
  */
 
 import { IAccountModel, INTERNAL_USER_GROUP, MatchController } from "@brontosaurus/db";
-import { ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
-import { Safe, SafeExtract } from '@sudoo/extract';
+import { createStringedBodyVerifyHandler, ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
 import { HTTP_RESPONSE_CODE } from "@sudoo/magic";
+import { createStrictMapPattern, createStringPattern, Pattern } from "@sudoo/pattern";
+import { fillStringedResult, StringedResult } from "@sudoo/verify";
 import { BrontosaurusRoute } from "../../handlers/basic";
 import { createAuthenticateHandler, createGroupVerifyHandler, createTokenHandler } from "../../handlers/handlers";
 import { autoHook } from "../../handlers/hook";
-import { ERROR_CODE } from "../../util/error";
+import { ERROR_CODE, panic } from "../../util/error";
 import { SafeToken } from "../../util/token";
 
 export type AccountGenerateTemporaryPasswordBody = {
@@ -19,6 +20,12 @@ export type AccountGenerateTemporaryPasswordBody = {
     readonly username: string;
     readonly namespace: string;
 };
+
+const bodyPattern: Pattern = createStrictMapPattern({
+
+    username: createStringPattern(),
+    namespace: createStringPattern(),
+});
 
 export class AccountGenerateTemporaryPasswordRoute extends BrontosaurusRoute {
 
@@ -29,12 +36,13 @@ export class AccountGenerateTemporaryPasswordRoute extends BrontosaurusRoute {
         autoHook.wrap(createTokenHandler(), 'Token'),
         autoHook.wrap(createAuthenticateHandler(), 'Authenticate'),
         autoHook.wrap(createGroupVerifyHandler([INTERNAL_USER_GROUP.SUPER_ADMIN]), 'Group Verify'),
+        autoHook.wrap(createStringedBodyVerifyHandler(bodyPattern), 'Body Verify'),
         autoHook.wrap(this._temporaryPasswordHandler.bind(this), 'Generate Temporary Password'),
     ];
 
     private async _temporaryPasswordHandler(req: SudooExpressRequest, res: SudooExpressResponse, next: SudooExpressNextFunction): Promise<void> {
 
-        const body: SafeExtract<AccountGenerateTemporaryPasswordBody> = Safe.extract(req.body as AccountGenerateTemporaryPasswordBody, this._error(ERROR_CODE.INSUFFICIENT_INFORMATION));
+        const body: AccountGenerateTemporaryPasswordBody = req.body;
 
         try {
 
@@ -42,12 +50,15 @@ export class AccountGenerateTemporaryPasswordRoute extends BrontosaurusRoute {
                 throw this._error(ERROR_CODE.TOKEN_INVALID);
             }
 
+            const verify: StringedResult = fillStringedResult(req.stringedBodyVerify);
+
+            if (!verify.succeed) {
+                throw panic.code(ERROR_CODE.REQUEST_DOES_MATCH_PATTERN, verify.invalids[0]);
+            }
+
             const token: SafeToken = req.principal;
             const requestUsername: string = token.body.directEnsure('username');
             const requestNamespace: string = token.body.directEnsure('namespace');
-
-            const username: string = body.directEnsure('username');
-            const namespace: string = body.directEnsure('namespace');
 
             const requestAccount: IAccountModel | null = await MatchController.getAccountByUsernameAndNamespaceName(requestUsername, requestNamespace);
 
@@ -55,10 +66,10 @@ export class AccountGenerateTemporaryPasswordRoute extends BrontosaurusRoute {
                 throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, requestUsername);
             }
 
-            const account: IAccountModel | null = await MatchController.getAccountByUsernameAndNamespaceName(username, namespace);
+            const account: IAccountModel | null = await MatchController.getAccountByUsernameAndNamespaceName(body.username, body.namespace);
 
             if (!account) {
-                throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, username);
+                throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, body.username);
             }
 
             const expireDate = new Date();
